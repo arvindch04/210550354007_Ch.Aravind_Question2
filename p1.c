ee277b43b1db581dbacee3bd586e8a8add9743fb
Ch.Aravind Kumar <aravindch0406@gmail.com>
2021-07-30 11:35:54 +0530

    210550354007_Ch.Aravind_Question2

---
+++ p1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <sys/mman.h>
+#include <sys/stat.h> /* For mode constants */
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int *count;
+
+int num;
+
+int main()
+{
+    int shmd;
+    shmd = shm_open("/key", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);  //Always remeber "/" before key if you forget that program will not work
+   //shmd is shared memory descriptor
+
+    ftruncate(shmd, sizeof(int)); //as memory is fixed
+    
+    count = (int *)mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, shmd, 0);
+    while (1)
+    {
+       = grep ^M  /usr/share/dict/words
+    }
+    return 0;
+}
